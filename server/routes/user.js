12b5f766f35df6fec01d5969e0b200097b07f7f9
Humayun <kobirhumayun@gmail.com>
2025-04-14 14:35:36 +0600

    feat: modify user router
    Add login, logout, refresh-token

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,4 +5,15 @@ const userController = require('../controllers/user');
 // User registration route
 router.post('/register', userController.registerUser);
 
+// User login route
+router.post('/login', userController.loginUser);
+
+// User logout route
+router.post('/logout', userController.logoutUser);
+
+// Refresh access token route
+router.post('/refresh-token', userController.refreshAccessToken);
+
+// Export the router
+
 module.exports = router;
