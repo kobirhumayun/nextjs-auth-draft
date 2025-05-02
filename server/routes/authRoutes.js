d9a032f3262aeab685e14edec1fc56b338b3e5e1
Humayun <kobirhumayun@gmail.com>
2025-05-03 00:07:47 +0600

    feat: add reload policy by API

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { authenticate } = require('../middleware/authMiddleware');
+const { authorize } = require('../middleware/casbinAuthorize');
+const { reloadPolicies } = require('../services/casbin');
 
 // Example of a protected route
+router.put('/reload-policies', authenticate, authorize("admin"), async (req, res) => {
+    try {
+        await reloadPolicies();
+        res.json({ message: 'Policies reloaded successfully.' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error reloading policies.', error: error.message });
+    }
+}
+)
 router.get('/user-info', authenticate, (req, res) => {
     res.json({ message: 'This is a protected route.', user: req.user });
 });
